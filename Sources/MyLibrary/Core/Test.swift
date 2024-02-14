fb70a5014a7248c8c6bb5e2ad8444a4e946edbdb
JinYu1998 <1870386153@qq.com>
2024-02-14 17:56:33 +0800

    first commit

---
+++ Sources/MyLibrary/Core/Test.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Terry Wei on 2024/2/14.
+//
+
+import Foundation
+
+public class Testxxx {
+    public init() {
+        print("init 123123213123123213 \n")
+    }
+    
+    public func TestxxxPrint() {
+        print("TestxxxPrint")
+    }
+}
---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public class TestxxxMyLibrary {
+    public init() {
+        print("init 123123213123123213 \n")
+    }
+    
+    public func TestxxxPrint() {
+        print("TestxxxPrint")
+    }
+}
